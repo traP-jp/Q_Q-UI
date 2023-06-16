e0a595e3b9d92ef386b2eff557b62ebfcc68e74a
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2023-06-17 00:54:25 +0900

    🌱 init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <app-header-vue />
+    <Suspense>
+      <template #default>
+        <router-view />
+      </template>
+      <template #fallback>
+        <loading />
+      </template>
+    </Suspense>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppHeaderVue from "./components/AppHeader.vue";
+</script>
+
+<style scoped></style>
---
+++ src/pages/[id].vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>ここに質問の詳細</p>
+    <p>{{ questionId }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useParam } from "/@/use/param";
+const questionId = useParam("id");
+</script>
+
+<style scoped></style>
