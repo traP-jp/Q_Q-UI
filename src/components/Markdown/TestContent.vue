9b08071c7b57939a8f51f9e489802bd4b7823680
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2023-06-17 05:18:00 +0900

    🚧 ✨ spoilerに対応

--- src/components/Markdown/TestContent.vue
+++ src/components/Markdown/TestContent.vue
@@ -1,11 +1,16 @@
 <template>
-  <markdown-content :content="content" />
+  <div @click="onClick">
+    <markdown-content :content="content" />
+  </div>
 </template>
 
 <script lang="ts" setup>
 import MarkdownContent from './MarkdownContent.vue'
+import { useMarkdownInternalHandler } from './useMarkdownInternalHandler'
 
 import { render } from '/@/lib/markdown/markdown'
 const rendered = await render('!!hogehoge!!')
 const content = rendered.renderedText
+
+const { onClick } = useMarkdownInternalHandler()
 </script>
