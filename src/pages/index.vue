5e6312cb9e8e157c41e4766ce0f59bdddb4c5421
24take <takechi12231223@gmail.com>
2023-06-17 13:08:57 +0900

    file_seiri

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,67 +1,13 @@
 <template>
-  <div class="SearchArea">
-  <input class="SearchBox" type="text" placeholder="検索" />
-  <div class="SearchButton"><button @click="Search">🔍</button></div>
-  </div>
+  <search-button />
 </template>
 
-
 <script setup lang="ts">
-
-  const SearchName = ''
-  const Search = (SearchName) => '1'
-
+import SearchButton from '/@/components/SearchButton.vue'
 </script>
 
 <style scoped>
-
-html{
+html {
   font-size: 16px;
 }
-.SearchArea {
-  display: block;
-  position: relative;
-  width: 500px;
-  height: 100px;
-  margin: 0 auto;
-}
-
-.SearchLabel {
-  position: absolute;
-  top: 15px;
-  left: 15px;
-  font-size: 1rem;
-  color: #a0a0a0;
-  transition: all 0.25s ease;
-}
-.SearchLabel.focus {
-  position: relative;
-  top: -25px;
-  left: 5px;
-  font-size: 14px;
-  color: #fff;
-  bottom: 2px;
-}
-
-.SearchBox {
-  position: relative;
-  display: block;
-  width: 500px;
-  margin-top: 50px;
-  padding: 15px;
-  border: solid 2px rgb(56, 50, 210);
-  border-radius: 10px;
-  font-size: 16px;
-  color: #000;
-  outline: none;
-}
-
-.SearchButton{
-  position: absolute;
-  right: -20px;
-  top: 26px;
-  font-size: 17px;
-  transform: translateY(-50%);
-}
-
 </style>
