4d7f7d20570f9161acee17932c5847f597b33a3b
24take <takechi12231223@gmail.com>
2023-06-17 14:55:54 +0900

    虫眼鏡アイコンを変更

--- src/components/SearchButton.vue
+++ src/components/SearchButton.vue
@@ -1,32 +1,37 @@
 <script setup lang="ts">
 const SearchName = ''
+import SearchSVG from '/@/assets/search.svg'
 </script>
 
 <template>
   <div class="SearchArea">
-    <div class="gr1">
-      <input
-        v-model="SearchName"
-        class="SearchBox"
-        type="text"
-        placeholder="検索"
-      />
-    </div>
-    <div class="SearchButton">🔍</div>
+    <input
+      v-model="SearchName"
+      class="SearchBox"
+      type="text"
+      placeholder="検索"
+    />
+    <img :src="SearchSVG" alt="検索" class="icon" />
+
+    <!-- <div class="SearchButton">🔍</div> -->
   </div>
 </template>
 
 <style scoped>
 .SearchArea {
+  margin: 0 auto;
+  max-width: 600px;
   display: grid;
   place-content: center;
   place-items: center;
+  justify-items: center;
+  align-items: center;
   padding-left: 7px;
-  grid-template-columns: 1fr 64px;
+  grid-template-columns: 1fr 50px;
   grid-template-rows: auto;
   border: solid 2px black;
   border-radius: 10px;
-  /* background-color: aqua; */
+  /* background-color: aqua;  */
 }
 
 .SearchBox {
@@ -40,8 +45,10 @@ const SearchName = ''
   outline: none;
 }
 
-.SearchButton {
-  font-size: 20px;
+.icon {
+  width: 24px;
+  height: 24px;
+  flex-shrink: 0;
   border: none;
 }
 </style>
