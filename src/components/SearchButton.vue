fb2fbb4104e9dc236452dcb58bcb9fddd5f2427b
24take <takechi12231223@gmail.com>
2023-06-17 14:22:45 +0900

    gridで書いた

--- src/components/SearchButton.vue
+++ src/components/SearchButton.vue
@@ -1,59 +1,47 @@
+<script setup lang="ts">
+const SearchName = ''
+</script>
+
 <template>
   <div class="SearchArea">
-    <input class="SearchBox" type="text" placeholder="検索" />
-    <div class="SearchButton"><button @click="Search">🔍</button></div>
+    <div class="gr1">
+      <input
+        v-model="SearchName"
+        class="SearchBox"
+        type="text"
+        placeholder="検索"
+      />
+    </div>
+    <div class="SearchButton">🔍</div>
   </div>
 </template>
 
-<script setup lang="ts">
-const SearchName = ''
-const Search = SearchName => '1'
-</script>
-
 <style scoped>
 .SearchArea {
-  display: block;
-  position: relative;
-  width: 500px;
-  height: 100px;
-  margin: 0 auto;
-}
-
-.SearchLabel {
-  position: absolute;
-  top: 15px;
-  left: 15px;
-  font-size: 1rem;
-  color: #a0a0a0;
-  transition: all 0.25s ease;
-}
-.SearchLabel.focus {
-  position: relative;
-  top: -25px;
-  left: 5px;
-  font-size: 14px;
-  color: #fff;
-  bottom: 2px;
+  display: grid;
+  place-content: center;
+  place-items: center;
+  padding-left: 7px;
+  grid-template-columns: 1fr 64px;
+  grid-template-rows: auto;
+  border: solid 2px black;
+  border-radius: 10px;
+  /* background-color: aqua; */
 }
 
 .SearchBox {
-  position: relative;
-  display: block;
   width: 500px;
-  margin-top: 50px;
-  padding: 15px;
-  border: solid 2px rgb(56, 50, 210);
-  border-radius: 10px;
+  padding-left: 20px;
+  padding-top: 15px;
+  padding-bottom: 15px;
+  border: none;
   font-size: 16px;
   color: #000;
   outline: none;
 }
 
 .SearchButton {
-  position: absolute;
-  right: -20px;
-  top: 26px;
-  font-size: 17px;
-  transform: translateY(-50%);
+  font-size: 20px;
+  border: none;
 }
 </style>
